9fa7a4407d96f27eaeed5166632d4475fe0d8ff6
Charles Lambret <charleslambretpro@gmail.com>
2023-08-02 19:30:12 +0200

    liste des articles

---
+++ okito/resources/js/components/Articlesliste.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <div v-for="article in articles" :key="article.id">
+        <h2>{{ article.nom }}</h2>
+        <p>{{ article.description }}</p>
+        <img :src="article.url" alt="Article image">
+        <p>Prix: {{ article.prix }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        articles: [],
+      };
+    },
+    async created() {
+      const response = await axios.get('/articles');
+      this.articles = response.data;
+    },
+  };
+  </script>
+  
