bf629d7601d00366fff26f01af3374aed0eb9613
Charles Lambret <charleslambretpro@gmail.com>
2023-08-02 19:11:42 +0200

    css premier composant

--- okito/resources/js/components/TableSelect.vue
+++ okito/resources/js/components/TableSelect.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-      <button v-for="number in 40" :key="number" @click="confirmTable(number)">
-        {{ number }}
-      </button>
+<div class="container w-50 p-3 shadow d-flex justify-content-center align-items-center my-auto mx-auto bg-white rounded flex-column" >
+    <h1>Sélectionnez votre table</h1>
+    <div class="flex-wrap overflow-auto">
+        <button 
+      v-for="number in 48" 
+      :key="number" 
+      @click="confirmTable(number)"
+      class="btn btn-outline-warning text-warning m-2"
+      style="width:6%"
+    >
+      {{ number }}
+    </button>
     </div>
+    
+  </div>
   </template>
   
   <script>
