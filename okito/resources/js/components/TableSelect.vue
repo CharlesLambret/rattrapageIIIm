b1cfa1a6b4b3515a57b40504f9fe64e7780489ca
Charles Lambret <charleslambretpro@gmail.com>
2023-08-02 17:31:40 +0200

    amélioration du premier composant

--- okito/resources/js/components/TableSelect.vue
+++ okito/resources/js/components/TableSelect.vue
@@ -1,51 +1,32 @@
+
+
 <template>
     <div>
-        <h2>Sélectionnez votre table</h2>
-        <select v-model="selectedTable">
-            <option v-for="table in tables" :key="table.id" :value="table.id">
-                {{ table.id }}
-            </option>
-        </select>
-
-        <button @click="createOrder">Confirmer</button>
+      <button v-for="number in 40" :key="number" @click="confirmTable(number)">
+        {{ number }}
+      </button>
     </div>
-</template>
-
-<script>
-import axios from 'axios'
-
-export default {
-    data() {
-        return {
-            tables: [],
-            selectedTable: null
-        }
-    },
-
-    mounted() {
-        this.getTables()
-    },
-
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  export default {
     methods: {
-        async getTables() {
-            try {
-                let response = await axios.get('/api/tables') 
-                this.tables = response.data
-            } catch (error) {
-                console.error(error)
-            }
-        },
-
-        async selectTable(table_id) {
-        axios.post(`/commandes/createWithTable/${table_id}`)
+      confirmTable(number) {
+        if (window.confirm(`Voulez-vous sélectionner la table ${number} ?`)) {
+          this.createOrder(number);
+        }
+      },
+      createOrder(number) {
+        axios.post(`/commandes/createWithTable/${number}`)
 
             .then(response => {
-                
+                console.log("commande créee")
             })
             .catch(error => {
-                
+                console.log("erreur", error)
             });
+      },
     },
-    }
-}
-</script>
+  };
+  </script>
